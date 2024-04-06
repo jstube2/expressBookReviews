0ff5b4ce167735d60afda459418d2b0d4bb6d5d1
jstube2 <jstube2@gmail.com>
2024-04-06 17:04:53 -0400

    task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,26 +25,33 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    //this.books = books;
-    //res.send(this.books)
-    Object.keys(books).forEach(key => {
-        console.log(`${books[key]}: ${books[key].author}`);
+    let book = {};
+    for (let key in books){
+        console.log(key);
         if(books[key].author == author){
-            res.send(books);
+            book[key] = books[key];
         }
-    });
+      }
+      res.send(book);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let book = {};
+    for (let key in books){
+        if(books[key].title == title){
+            book[key] = books[key];
+        }
+      }
+      res.send(book);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    console.log(isbn);
+    res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
