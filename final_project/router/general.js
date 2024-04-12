6e042feb675a23b1ce29c8744aa3368fa7d5123a
jstube2 <jstube2@gmail.com>
2024-04-12 13:27:37 -0400

    Final Updates

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,68 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books,null,4));
+  
+  const p1 = new Promise((resolve, reject) => {
+    resolve(JSON.stringify(books,null,4));
+    reject("Error");
+  });
+  
+  p1.then(
+    (value) => {
+    res.send(value);
+      console.log(value); // Success!
+    },
+    (reason) => {
+      console.log(reason); // Error!
+    },
+  );
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn])
+    //res.send(books[isbn])
+    const p1 = new Promise((resolve, reject) => {
+        resolve(books[isbn]);
+        reject("Error");
+      });
+      
+      p1.then(
+        (value) => {
+        res.send(value);
+          console.log(value); // Success!
+        },
+        (reason) => {
+          console.log(reason); // Error!
+        },
+      );
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    let book = {};
+    
+    const p1 = new Promise((resolve, reject) => {
+        let book = {};
+        for (let key in books){
+            console.log(key);
+            if(books[key].author == author){
+                resolve(book[key] = books[key]);
+            }
+          };
+        reject("Error");
+      });
+      
+      p1.then(
+        (value) => {
+        res.send(value);
+          console.log(value); // Success!
+        },
+        (reason) => {
+          console.log(reason); // Error!
+        },
+      );
+    /*
     for (let key in books){
         console.log(key);
         if(books[key].author == author){
@@ -33,18 +82,43 @@ public_users.get('/author/:author',function (req, res) {
         }
       }
       res.send(book);
+      */
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
+    const p1 = new Promise((resolve, reject) => {
+        console.log(title);
+        let book = {};
+        for (let key in books){
+            console.log(key);
+            if(books[key].title == title){
+                resolve(book[key] = books[key]);
+            }
+          };
+        reject("Error");
+      });
+      
+      p1.then(
+        (value) => {
+        res.send(value);
+          console.log(value); // Success!
+        },
+        (reason) => {
+          console.log(reason); // Error!
+        },
+      );
+    /*
     let book = {};
+
     for (let key in books){
         if(books[key].title == title){
             book[key] = books[key];
         }
       }
       res.send(book);
+      */
 });
 
 //  Get book review
